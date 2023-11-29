997b80bc9b0e7474a3effcfe321283a509efd9b4
Farrel Muhammad <farrel.iqbal@creontek.com>
2023-11-29 10:50:48 +0700

    Hide route blog and about on header and change to coming soon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Home | With Farrel';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'With Farrel';
+export const SITE_DESCRIPTION = '🧑‍🚀 Hello, I am Farrel Muhammad (Front-End Developer). Passionate about expanding experience in startup companies. With a proven track record of collaborating effectively with a great user experience.';
